d8d379e01db8566b957f753006f5f174ca1ca211
Anthony Jackman <energeticpixels@gmail.com>
2019-04-14 21:19:21 -0500

    kills pet

--- scripts/game.js
+++ scripts/game.js
@@ -78,6 +78,9 @@ var GameState = {
 
     this.refreshStats();
 
+    // decrease healthevery 5 seconds
+    this.statsDecreaser = this.game.time.events.loop(Phaser.Timer.SECOND * 5, this.reduceProperties, this);
+
   },
 
   pickItem: function(sprite, event) {
@@ -159,7 +162,25 @@ var GameState = {
   refreshStats: function() {
     this.healthText.text = this.pet.customParams.health;
     this.funText.text = this.pet.customParams.fun;
-  };
+  },
+
+  reduceProperties: function() {
+    this.pet.customParams.health -= 10;
+    this.pet.customParams.fun -= 15;
+    this.refreshStats();
+  },
+
+  gameOver: function() {
+    this.game.state.restart();
+  },
+
+  update: function() {
+    if(this.pet.customParams.health <= 0 || this.pet.customParams.fun <= 0) {
+      this.pet.frame = 4;
+      this.uiBlocked = true;
+      this.game.time.events.add(2000, this.gameOver, this);
+    }
+  }
 };
 
 // initiate the framework
