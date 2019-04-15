40519a7f7e8f722ec743ef52f57cf9272a94d8f0
Anthony Jackman <energeticpixels@gmail.com>
2019-04-14 20:41:06 -0500

    adds placing new items on screen

--- scripts/game.js
+++ scripts/game.js
@@ -24,6 +24,8 @@ var GameState = {
   // execute after everything is loaded
   create: function() {
     this.background = this.game.add.sprite(0, 0, 'backyard');
+    this.background.inputEnabled = true;
+    this.background.events.onInputDown.add(this.placeItem, this);
 
     this.pet = this.game.add.sprite(100, 400, 'pet');
     this.pet.anchor.setTo(0.5);
@@ -101,6 +103,18 @@ var GameState = {
       element.alpha = 1;
     });
     this.selectedItem = null;
+  },
+
+  placeItem: function(sprite, event) {
+    if(this.selectedItem && !this.uiBlocked) {
+      var x = event.position.x;
+    var y = event.position.y;
+
+    var newItem = this.game.add.sprite(x, y, this.selectedItem.key);
+    newItem.anchor.setTo(0.5);
+    newItem.customParams = this.selectedItem.customParams;
+
+    }
   }
 };
 
